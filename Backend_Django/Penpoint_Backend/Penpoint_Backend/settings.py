a37c5ddc762c049f91536478d8f499f0c86b7637
Rafael Quizon <rafael.quizon.c@gmail.com>
2025-04-10 13:39:50 +0800

    Added Models for Database

--- Backend_Django/Penpoint_Backend/Penpoint_Backend/settings.py
+++ Backend_Django/Penpoint_Backend/Penpoint_Backend/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'apis'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+    ]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,6 +86,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+AUTH_USER_MODEL = 'apis.Users'
 
 
 # Password validation
--- Backend_Django/Penpoint_Backend/apis/models.py
+++ Backend_Django/Penpoint_Backend/apis/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Users(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+class Blog(models.Model):
+    blogID = models.AutoField(primary_key=True)
+    blogTitle = models.CharField(max_length=100)
+    blogContent = models.CharField(max_length=20000)
+    blogCreatedat = models.DateField()
+    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.blogTitle
+
+class News(models.Model):
+    newsID = models.AutoField(primary_key=True)
+    newsTitle = models.CharField(max_length=2048)
+    newsFrom = models.CharField(max_length=100)
+    newsDate = models.DateField()
+    newsDescription = models.CharField(max_length=20000)
+    newsLink = models.CharField(max_length=200)
+    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.newsTitle
+    
